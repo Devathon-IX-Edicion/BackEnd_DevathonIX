72600b5aad309d641c1d696eef666e0225a0bf63
Mario <109123293+darkmariod@users.noreply.github.com>
2025-03-27 21:04:08 -0500

    feat: category first migration

--- category/models.py
+++ category/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Category(models.Model):   
+    name_category = models.CharField(max_length=100, null=False)
+    description   = models.CharField(max_length=100, null=False)
+    date_created  = models.DateTimeField(auto_now_add=True)
+    slug = AutoSlugField(populate_from='name_category')
+
+    def __str__(self):
+        return self.name_category  
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
--- category/views.py
+++ category/views.py
@@ -1,7 +1,15 @@
 from rest_framework.views import APIView
+from .models import *
 from django.http.response import JsonResponse
+from rest_framework.response import Response
+from .serializers import *
+from http import HTTPStatus
 
 
 class ClassName(APIView):
     def get(self, request):
-        return Response({"message": "Hello, world!"})
+        # SELECT * from  category order by desc:
+        data = Category.objects.order_by('-id').all()
+        json_data =  CategorySerializer(data, many=True)
+        #return Response(date_json.data)
+        return  JsonResponse({"data":json_data.data}, HTTPStatus.OK)
