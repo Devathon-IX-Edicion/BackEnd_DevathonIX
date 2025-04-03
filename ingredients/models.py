c8f4a38c921ea9d390c20b5aa0a518aba223a8bc
StefanoIG <stefano20033@gmail.com>
2025-04-03 00:16:48 -0500

    feat: add Ingredient model with fields for name, quantity, and unit Note: This model and serializer is are burned data, waiting for the official one to be made

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Modelo de prueba
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.FloatField()
+    unit = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
