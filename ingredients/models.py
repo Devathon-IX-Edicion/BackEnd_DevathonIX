1111e1c9611f0e68512c5de39847f6dcd30c8fa6
StefanoIG <stefano20033@gmail.com>
2025-04-21 21:22:12 -0500

    feat: refactor ingredient and category models, add migration for category model

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,165 +1,24 @@
-from rest_framework.views import APIView
-from django.http.response import JsonResponse
-from http import HTTPStatus
-from django.http import Http404
-from .models import Ingredient, Category
-from django.utils.dateformat import DateFormat
+from django.db import models
 
-MAGIC_LEVEL_CHOICES = (
-    ("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5"),
-    ("6", "6"), ("7", "7"), ("8", "8"), ("9", "9"), ("10", "10"),
-)
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
 
-class IngredientList(APIView):
-    
-    def get(self, request):
-        ingredients = Ingredient.objects.order_by('-id').all()
-        data = [{
-            "id": ing.id,
-            "name": ing.name,
-            "slug": ing.slug,
-            "description": ing.description,
-            "magic_level": ing.magic_level,
-            "preparation_time": ing.preparation_time,
-            "category": ing.category.name,
-            "date_created": DateFormat(ing.date_created).format('d/m/Y')
-        } for ing in ingredients]
-        return JsonResponse({"data": data}, safe=False)
-    
-    def post(self, request):
-        required_fields = ['name', 'preparation_time', 'description', 'category_id', 'magic_level']
-        for field in required_fields:
-            if not request.data.get(field):
-                return JsonResponse(
-                    {"status": "error", "message": f"The {field} field is required"},
-                    status=HTTPStatus.BAD_REQUEST
-                )
+    def __str__(self):
+        return self.name
 
-        try:
-            category = Category.objects.get(pk=request.data["category_id"])
-        except Category.DoesNotExist:
-            return JsonResponse(
-                {"status": "error", "message": "Invalid category ID"},
-                status=HTTPStatus.BAD_REQUEST
-            )
 
-        if request.data["magic_level"] not in [choice[0] for choice in MAGIC_LEVEL_CHOICES]:
-            return JsonResponse(
-                {"status": "error", "message": "Invalid magic level value"},
-                status=HTTPStatus.BAD_REQUEST
-            )
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    magic_level = models.CharField(max_length=2, choices=[
+        ("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"), ("5", "5"),
+        ("6", "6"), ("7", "7"), ("8", "8"), ("9", "9"), ("10", "10"),
+    ])
+    preparation_time = models.PositiveIntegerField()  # Time in minutes
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="ingredients")
+    date_created = models.DateTimeField(auto_now_add=True)
 
-        if Ingredient.objects.filter(name=request.data["name"]).exists():
-            return JsonResponse(
-                {"status": "error", "message": "Ingredient name already exists"},
-                status=HTTPStatus.BAD_REQUEST
-            )
-
-        try:
-            Ingredient.objects.create(
-                name=request.data["name"],
-                preparation_time=request.data["preparation_time"],
-                description=request.data["description"],
-                category=category,
-                magic_level=request.data["magic_level"]
-            )
-            return JsonResponse(
-                {"status": "success", "message": "Ingredient created successfully"},
-                status=HTTPStatus.CREATED
-            )
-        except Exception as e:
-            return JsonResponse(
-                {"status": "error", "message": str(e)},
-                status=HTTPStatus.INTERNAL_SERVER_ERROR
-            )
-
-class IngredientDetail(APIView):
-    
-    def get(self, request, id):
-        try:
-            ingredient = Ingredient.objects.get(id=id)
-            data = {
-                "id": ingredient.id,
-                "name": ingredient.name,
-                "slug": ingredient.slug,
-                "description": ingredient.description,
-                "magic_level": ingredient.magic_level,
-                "preparation_time": ingredient.preparation_time,
-                "category_id": ingredient.category.id,
-                "category": ingredient.category.name,
-                "date_created": DateFormat(ingredient.date_created).format('d/m/Y')
-            }
-            return JsonResponse({"data": data}, status=HTTPStatus.OK)
-        except Ingredient.DoesNotExist:
-            return JsonResponse(
-                {"status": "error", "message": "Ingredient not found"},
-                status=HTTPStatus.NOT_FOUND
-            )
-    
-    def put(self, request, id):
-        try:
-            ingredient = Ingredient.objects.get(id=id)
-        except Ingredient.DoesNotExist:
-            return JsonResponse(
-                {"status": "error", "message": "Ingredient not found"},
-                status=HTTPStatus.NOT_FOUND
-            )
-
-        required_fields = ['name', 'preparation_time', 'description', 'category_id', 'magic_level']
-        for field in required_fields:
-            if not request.data.get(field):
-                return JsonResponse(
-                    {"status": "error", "message": f"The {field} field is required"},
-                    status=HTTPStatus.BAD_REQUEST
-                )
-
-        try:
-            category = Category.objects.get(pk=request.data["category_id"])
-        except Category.DoesNotExist:
-            return JsonResponse(
-                {"status": "error", "message": "Invalid category ID"},
-                status=HTTPStatus.BAD_REQUEST
-            )
-
-        if request.data["magic_level"] not in [choice[0] for choice in MAGIC_LEVEL_CHOICES]:
-            return JsonResponse(
-                {"status": "error", "message": "Invalid magic level value"},
-                status=HTTPStatus.BAD_REQUEST
-            )
-
-        if Ingredient.objects.filter(name=request.data["name"]).exclude(id=id).exists():
-            return JsonResponse(
-                {"status": "error", "message": "Ingredient name already exists"},
-                status=HTTPStatus.BAD_REQUEST
-            )
-
-        try:
-            ingredient.name = request.data["name"]
-            ingredient.preparation_time = request.data["preparation_time"]
-            ingredient.description = request.data["description"]
-            ingredient.category = category
-            ingredient.magic_level = request.data["magic_level"]
-            ingredient.save()
-            return JsonResponse(
-                {"status": "success", "message": "Ingredient updated successfully"},
-                status=HTTPStatus.OK
-            )
-        except Exception as e:
-            return JsonResponse(
-                {"status": "error", "message": str(e)},
-                status=HTTPStatus.INTERNAL_SERVER_ERROR
-            )
-    
-    def delete(self, request, id):
-        try:
-            ingredient = Ingredient.objects.get(id=id)
-            ingredient.delete()
-            return JsonResponse(
-                {"status": "success", "message": "Ingredient deleted successfully"},
-                status=HTTPStatus.OK
-            )
-        except Ingredient.DoesNotExist:
-            return JsonResponse(
-                {"status": "error", "message": "Ingredient not found"},
-                status=HTTPStatus.NOT_FOUND
-            )
+    def __str__(self):
+        return self.name
